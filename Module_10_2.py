bfab95ec3b63223dd4141613eb89445696cc921e
dmitrbp <80584067+dmitrbp@users.noreply.github.com>
2024-08-15 00:50:38 +0300

    release 0

--- Module_10_2.py
+++ Module_10_2.py
@@ -14,14 +14,14 @@ class Knight(Thread):
         days = 0
         while self.enemies > 0:
             days += 1
-            self.enemies -= self.power
+            self.enemies = self.enemies - self.power if self.enemies - self.power > 0  else 0
             print(f'{self.name} сражается {days} день(дня)..., осталось {self.enemies} воинов.')
             sleep(1)
         print(f'{self.name} одержал победу спустя {days} дней(дня)!')
 
 
 # Создание класса
-first_knight = Knight('Sir Lancelot', 10)
+first_knight = Knight('Sir Lancelot', 45)
 second_knight = Knight("Sir Galahad", 20)
 # Запуск потоков и остановка текущего
 first_knight.start()
